d9d972ee9a411040ec7a72c6bbaa96aaf2d52997
Efim <jacober@list.ru>
2025-11-28 14:23:33 +0300

    start add functional main menu

--- mobile application/MyGame/app/src/main/java/com/example/mygame/MainActivity.java
+++ mobile application/MyGame/app/src/main/java/com/example/mygame/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mygame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    void init(){
+        Button go_play=findViewById(R.id.go_play);
+        EditText input_name=findViewById(R.id.input_name);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+    }
+
+    public void activity_store_direct(View view) {
+        Intent intent = new Intent(this, activity_store.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
 
     }
 }
